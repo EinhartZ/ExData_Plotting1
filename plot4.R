63c1e3ab3a44a23c06620781230585abcf7c2693
Einhart <Einhart.zhang@gmail.com>
2015-10-11 21:05:51 +0200

    delete leg border

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ plot(data$Time, data$Sub_metering_1, type="n", xlab = "", ylab = "Energy sub met
 lines(data$Time, data$Sub_metering_1, col="black")
 lines(data$Time, data$Sub_metering_2, col="red")
 lines(data$Time, data$Sub_metering_3, col="blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col=c("black", "red","blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col=c("black", "red","blue"), bg = "transparent", bty = "n")
 #(2, 2)
 plot(data$Time, data$Global_reactive_power, type="l", xlab = "datetime", ylab = "Global_reactive_power");
 dev.off();
